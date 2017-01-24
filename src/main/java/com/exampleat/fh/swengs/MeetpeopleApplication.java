a88861916281fb1deca3d4a05394d93ee11bd16b
loete <stefan.ltnr@gmail.com>
2017-01-24 09:45:18 +0100

    initial commit

---
+++ src/main/java/com/exampleat/fh/swengs/MeetpeopleApplication.java
@@ -0,0 +1,12 @@
+package com.exampleat.fh.swengs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetpeopleApplication.class, args);
+	}
+}
